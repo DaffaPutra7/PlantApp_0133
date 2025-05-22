dcf52c70a3847c45486edfb0949aba75d6f4854e
DaffaPutra7 <putradika3604@gmail.com>
2025-05-23 01:30:59 +0700

    menambah code untuk membuat dan mengatur textbutton

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp_133/constants.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:plantapp_133/constants.dart';
 // import 'package:plantapp_133/screens/details/components/icon_cart.dart';
@@ -19,7 +20,47 @@ class BodyDetails extends StatelessWidget {
             title: "Angelica",
             country: "Russia",
             price: 440,
-          )
+          ),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget> [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20)
+                      )
+                    )
+                  ),
+                  onPressed: () {}, 
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  )
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  
+                  onPressed: () {}, 
+                  child: Text(
+                    "Description",
+                    style: TextStyle(
+                      color: kPrimaryColor,
+                      fontSize: 16
+                    ),
+                  )
+                )
+              )
+            ],
+          ),
         ],
       ),
     );
