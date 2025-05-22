9b457d1ad0dd60a965439ed2e76c367f8f656c5d
DaffaPutra7 <putradika3604@gmail.com>
2025-05-22 23:32:55 +0700

    menambah code untuk mengatur icon

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp_133/constants.dart';
 
 class BodyDetails extends StatelessWidget {
@@ -9,34 +10,53 @@ class BodyDetails extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: <Widget> [
-        Row(
-          children: <Widget> [
-            Expanded(
-              child: Column(),
-            ),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63)
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: <Widget> [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
+                  child: Column(
+                    children: <Widget> [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }, 
+                          icon: SvgPicture.asset("assets/icons/back_arrow.svg", height: 50, width: 50),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 60,
-                    color: kPrimaryColor.withOpacity(0.30)
-                  )
-                ],
-                image: DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/images/image.jpg") 
-                )
               ),
-            )
-          ],
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(63),
+                    bottomLeft: Radius.circular(63)
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 60,
+                      color: kPrimaryColor.withOpacity(0.30)
+                    )
+                  ],
+                  image: DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/image.jpg") 
+                  )
+                ),
+              )
+            ],
+          ),
         )
       ],
     );
