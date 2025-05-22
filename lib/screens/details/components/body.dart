e9b5d4df330b8fe7ba41eab3f461c9f32424fb43
DaffaPutra7 <putradika3604@gmail.com>
2025-05-23 00:38:26 +0700

    memindahkan class ImageAndIcons ke file baru dan melakukan import di file body.dart

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp_133/constants.dart';
-import 'package:plantapp_133/screens/details/components/icon_cart.dart';
+// import 'package:plantapp_133/screens/details/components/icon_cart.dart';
+import 'package:plantapp_133/screens/details/components/image_and_icons.dart';
 
 class BodyDetails extends StatelessWidget {
   const BodyDetails({super.key});
@@ -47,38 +48,6 @@ class BodyDetails extends StatelessWidget {
               ),
             ),
           )
-        ],
-      ),
-    );
-  }
-}
-
-class ImageAndIcons extends StatelessWidget {
-  const ImageAndIcons({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
-      child: Column(
-        children: <Widget> [
-          Align(
-            alignment: Alignment.topLeft,
-            child: IconButton(
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              onPressed: () {
-                Navigator.pop(context);
-              }, 
-              icon: SvgPicture.asset("assets/icons/back_arrow.svg", height: 50, width: 50),
-            ),
-          ),
-          Spacer(),
-          IconCard(icon: "assets/icons/sun.svg"),
-          IconCard(icon: "assets/icons/thermometer.svg"),
-          IconCard(icon: "assets/icons/water-drop.svg"),
-          IconCard(icon: "assets/icons/wind.svg"),
         ],
       ),
     );
