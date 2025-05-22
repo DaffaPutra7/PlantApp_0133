28ac7624570f5fc2bef491e5e0566359b98379f5
DaffaPutra7 <putradika3604@gmail.com>
2025-05-22 18:51:10 +0700

    memindahkan class TitleWithMoreButton ke file baru dan melakukan import di file body.dart

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:plantapp_133/constants.dart';
+// import 'package:plantapp_133/constants.dart';
 // import 'package:flutter_svg/svg.dart';
 // import 'package:plantapp_133/constants.dart';
 import 'package:plantapp_133/screens/components/header_with_seacrhbox.dart';
+import 'package:plantapp_133/screens/components/title_with_more_button.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -23,80 +24,6 @@ class Body extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class TitleWithMoreButton extends StatelessWidget {
-  const TitleWithMoreButton({
-    required this.title,
-    required this.press,
-    super.key,
-  });
-
-  final String title;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Row(
-        children: [
-          TextWithCustomUnderline(text: title),
-          Spacer(),
-          TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: kPrimaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-            onPressed: press,
-            child: Text(
-              "More", 
-              style: TextStyle(color: Colors.white)
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TextWithCustomUnderline extends StatelessWidget {
-  const TextWithCustomUnderline({
-    required this.text,
-    super.key,
-  });
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: EdgeInsets.only(right: kDefaultPadding / 4),
-              height: 7,
-              color: kPrimaryColor.withOpacity(0.2),
-            ),
-          ),
-        ],
       ),
     );
   }
