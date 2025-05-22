bbcdec0e46a540f37c9761445fad5ad76949738f
DaffaPutra7 <putradika3604@gmail.com>
2025-05-22 21:58:26 +0700

    memindahkan class FeaturedPlants dan FeaturePlantCard ke file baru dan melakukan import di file body.dart

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plantapp_133/constants.dart';
+import 'package:plantapp_133/screens/components/featured_plants.dart';
 // import 'package:plantapp_133/constants.dart';
 // import 'package:plantapp_133/constants.dart';
 // import 'package:flutter_svg/svg.dart';
@@ -27,66 +28,6 @@ class Body extends StatelessWidget {
             FeaturedPlants(),
             SizedBox(height: kDefaultPadding),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class FeaturedPlants extends StatelessWidget {
-  const FeaturedPlants({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: <Widget> [
-          FeaturePlantCard(
-            image: "assets/images/bottom_image1.jpg",
-            press: () {},
-          ),
-          FeaturePlantCard(
-            image: "assets/images/bottom_image2.jpg",
-            press: () {},
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FeaturePlantCard extends StatelessWidget {
-  const FeaturePlantCard({
-    required this.image,
-    required this.press,
-    super.key,
-  });
-
-  final String image;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.only(
-          left: kDefaultPadding,
-          top: kDefaultPadding / 2,
-          bottom: kDefaultPadding / 2,
-        ),
-        width: size.width * 0.8,
-        height: 185,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(image)
-          )
         ),
       ),
     );
