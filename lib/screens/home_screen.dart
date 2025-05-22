ed2c22735141f4217eada96420edda1d49da5533
DaffaPutra7 <putradika3604@gmail.com>
2025-05-22 22:28:25 +0700

    memindahkan class MyBottomNavbar ke file baru dan melakukan import di file home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:plantapp_133/constants.dart';
+import 'package:plantapp_133/components/my_bottom_navbar.dart';
+// import 'package:plantapp_133/constants.dart';
 import 'package:plantapp_133/screens/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -21,51 +22,6 @@ class HomeScreen extends StatelessWidget {
       leading: IconButton(
         onPressed: () {}, 
         icon: SvgPicture.asset("assets/icons/menu.svg")
-      ),
-    );
-  }
-}
-
-class MyBottomNavbar extends StatelessWidget {
-  const MyBottomNavbar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        left: kDefaultPadding * 2,
-        right: kDefaultPadding * 2,
-        bottom: kDefaultPadding,
-      ),
-      height: 80,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, -10),
-            blurRadius: 35,
-            color: kPrimaryColor.withOpacity(0.30)
-          )
-        ],
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget> [
-          IconButton(
-            onPressed: () {}, 
-            icon: SvgPicture.asset("assets/icons/flower.svg", height: 25, width: 25)
-          ),
-          IconButton(
-            onPressed: () {}, 
-            icon: SvgPicture.asset("assets/icons/heart_icon.svg", height: 25, width: 25)
-          ),
-          IconButton(
-            onPressed: () {}, 
-            icon: SvgPicture.asset("assets/icons/user_icon.svg", height: 25, width: 25)
-          ),
-        ],
       ),
     );
   }
