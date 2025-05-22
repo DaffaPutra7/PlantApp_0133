274047bbc8dd1a624732f52f9ec5cc8af970874e
DaffaPutra7 <putradika3604@gmail.com>
2025-05-22 23:05:00 +0700

    menambah file baru dan diimport di details_screen.dart

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BodyDetails extends StatelessWidget {
+  const BodyDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp_133/screens/details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: BodyDetails(),
+    );
   }
 }
