c73cba46fd9fec5d3b332b90dfc9026de5d54371
DaffaPutra7 <putradika3604@gmail.com>
2025-05-23 00:03:18 +0700

    me refactor dengan menggunakan extract widget IconCard dan buat icon nya jadi empat

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -30,29 +30,10 @@ class BodyDetails extends StatelessWidget {
                         ),
                       ),
                       Spacer(),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
-                        padding: EdgeInsets.all(kDefaultPadding / 2),
-                        height: 62,
-                        width: 62,
-                        decoration: BoxDecoration(
-                          color: kBackgroundColor,
-                          borderRadius: BorderRadius.circular(6),
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0, 15),
-                              blurRadius: 22,
-                              color: kPrimaryColor.withOpacity(0.22),
-                            ),
-                            BoxShadow(
-                              offset: Offset(-15, -15),
-                              blurRadius: 20,
-                              color: Colors.white
-                            ),
-                          ],
-                        ),
-                        child: SvgPicture.asset("assets/icons/sun.svg", color: kPrimaryColor,),
-                      )
+                      IconCard(icon: "assets/icons/sun.svg"),
+                      IconCard(icon: "assets/icons/thermometer.svg"),
+                      IconCard(icon: "assets/icons/water-drop.svg"),
+                      IconCard(icon: "assets/icons/wind.svg"),
                     ],
                   ),
                 ),
@@ -83,6 +64,43 @@ class BodyDetails extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class IconCard extends StatelessWidget {
+  const IconCard({
+    required this.icon,
+    super.key,
+  });
+
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
+      padding: EdgeInsets.all(kDefaultPadding / 2),
+      height: 62,
+      width: 62,
+      decoration: BoxDecoration(
+        color: kBackgroundColor,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 15),
+            blurRadius: 22,
+            color: kPrimaryColor.withOpacity(0.22),
+          ),
+          BoxShadow(
+            offset: Offset(-15, -15),
+            blurRadius: 20,
+            color: Colors.white
+          ),
+        ],
+      ),
+      child: SvgPicture.asset(icon, color: kPrimaryColor,),
     );
   }
 }
