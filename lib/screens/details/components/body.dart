6b347e5682e33483be836d96df1b97332eb0c525
DaffaPutra7 <putradika3604@gmail.com>
2025-05-22 23:19:27 +0700

    menambah code untuk mengatur gambar

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp_133/constants.dart';
 
 class BodyDetails extends StatelessWidget {
   const BodyDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget> [
+        Row(
+          children: <Widget> [
+            Expanded(
+              child: Column(),
+            ),
+            Container(
+              height: size.height * 0.8,
+              width: size.width * 0.75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(63),
+                  bottomLeft: Radius.circular(63)
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 60,
+                    color: kPrimaryColor.withOpacity(0.30)
+                  )
+                ],
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/image.jpg") 
+                )
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
