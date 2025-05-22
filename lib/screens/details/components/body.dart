4b500baaf51d3e8c91bfde4f2d41e788c3f17fa2
DaffaPutra7 <putradika3604@gmail.com>
2025-05-23 00:06:31 +0700

    memindahkan class IconCard ke file baru dan melakukan import di file body.dart

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp_133/constants.dart';
+import 'package:plantapp_133/screens/details/components/icon_cart.dart';
 
 class BodyDetails extends StatelessWidget {
   const BodyDetails({super.key});
@@ -64,43 +65,6 @@ class BodyDetails extends StatelessWidget {
           ),
         )
       ],
-    );
-  }
-}
-
-class IconCard extends StatelessWidget {
-  const IconCard({
-    required this.icon,
-    super.key,
-  });
-
-  final String icon;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
-      padding: EdgeInsets.all(kDefaultPadding / 2),
-      height: 62,
-      width: 62,
-      decoration: BoxDecoration(
-        color: kBackgroundColor,
-        borderRadius: BorderRadius.circular(6),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 15),
-            blurRadius: 22,
-            color: kPrimaryColor.withOpacity(0.22),
-          ),
-          BoxShadow(
-            offset: Offset(-15, -15),
-            blurRadius: 20,
-            color: Colors.white
-          ),
-        ],
-      ),
-      child: SvgPicture.asset(icon, color: kPrimaryColor,),
     );
   }
 }
